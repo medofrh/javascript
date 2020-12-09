0446e79525ccb8b8be5ff4e46dc2032202bc8766
unknown <medo0o0frh@gmail.com>
2020-12-09 19:26:48 +0100

    Myfirst Javascript

---
+++ module4-solution/SpeakGoodBye.js
@@ -0,0 +1,11 @@
+(function(window){
+
+var byeSpeaker ={},
+speakWord = "Good Bye";
+
+byeSpeaker.speak= function (name) {
+  console.log(speakWord + " " +name);
+};
+
+window.byeSpeaker = byeSpeaker ;
+})(window);
